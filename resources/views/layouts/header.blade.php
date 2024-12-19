f7e700511af78e99ef2e123789f305d44855c0fa
RicarGit <ricardoagava@gmail.com>
2024-12-19 01:03:09 -0300

    feat: adding header and render it in home

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,9 @@
+<header class="fixed top-0 z-10 flex h-20 w-full items-center bg-white font-semibold text-gray-500 shadow-md">
+  <div class="container mx-auto flex items-center justify-between px-5">
+    <a href="{{ route('home') }}" class="text-2xl font-bold">Filmes App</a>
+    <div class="flex items-center gap-4">
+      <p>Usuário</p>
+      <div class="size-10 rounded-[50%] bg-gray-700"></div>
+    </div>
+  </div>
+</header>
