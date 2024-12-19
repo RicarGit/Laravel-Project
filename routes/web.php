b9056db49642dfc6cc75f7322ca26b05ecbb6870
RicarGit <ricardoagava@gmail.com>
2024-12-19 01:31:18 -0300

    feat: adding movie details route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MovieDetailsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/movie/{id}', [MovieDetailsController::class, 'movieDetails'])->name('details');
