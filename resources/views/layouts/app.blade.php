1003c5e87d1e218bb420fa20ac80a138f0c748ec
RicarGit <ricardoagava@gmail.com>
2024-12-19 01:01:26 -0300

    feat: activate tailwind css with vite and add styles in home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  {{-- @vite('resources/css/app.css') --}}
+  @vite('resources/css/app.css')
   <title>Home</title>
 </head>
 
+<body class="bg-[#ddd] pt-20">
 <body>
 
-  <main>
+  <main class="container mx-auto px-5">
 
     @yield('content')
 
