90540feac05471c6ee196f5cb8b9e13cae2493e5
RicarGit <ricardoagava@gmail.com>
2024-12-20 17:01:05 -0300

    feat: adding filters to controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,20 +12,42 @@ class HomeController extends Controller {
     $this->tmdbService = $tmdbService;
   }
 
+  public function index(Request $request) {
   public function index() {
     $movies = $this->tmdbService->getMovies();
 
     return view('home', ['movies' => $movies['results'] ?? []]);
+    $filters = [
+      'popular' => 'Populares',
+      'top_rated' => 'Mais Votados',
+      'upcoming' => 'Próximos Lançamentos',
+      'now_playing' => 'Lançamentos Recentes',
+    ];
+
+    $movieFilter = $request->query('filter', 'popular');
     $genreName = $request->query('genre', '');
     $movieName = $request->query('movieName', '');
+    $currentPage = $request->query('page', 1);
+
+    if ($movieName) {
+      $movies = $this->tmdbService->searchMoviesByName($movieName, $currentPage);
+    } elseif ($genreName) {
+      $movies = $this->tmdbService->getMoviesByGenre($genreName, $currentPage);
+    } else {
+      $movies = $this->tmdbService->getMovies($movieFilter, $currentPage);
+    }
+
     $genres = $this->tmdbService->getMovieGenres();
 
     return view(
       'home',
       [
         'movies' => $movies['results'] ?? [],
+        'movieFilter' => $movieFilter,
+        'filters' => $filters,
         'genres' => $genres,
         'genreName' => $genreName,
+        'movieName' => $movieName,
       ]
     );
   }
