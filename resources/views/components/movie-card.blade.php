f86a4956146247e958722c97a88037f94d7ddf9f
RicarGit <ricardoagava@gmail.com>
2024-12-20 15:44:37 -0300

    feat: adding fn to limite movie title to 23 chars also add image title prop to images

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -1,16 +1,18 @@
-<ul class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+<ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
 
   @foreach ($movies as $movie)
     <a href="{{ route('details', ['id' => $movie['id']]) }}">
-      <li class="bg-white shadow-md rounded-lg overflow-hidden hover:scale-105 transition-all hover:cursor-pointer">
+      <li class="overflow-hidden rounded-lg bg-white shadow-md transition-all hover:scale-105 hover:cursor-pointer">
         <img
           src="https://image.tmdb.org/t/p/w500{{ $movie['poster_path'] }}"
           alt="{{ $movie['title'] }}"
-          class="w-full h-[400px]"
+          class="h-[400px] w-full"
+          title="{{ $movie['title'] }}"
         >
         <div class="p-4">
-          <h2 class="text-lg font-bold">{{ $movie['title'] }}</h2>
-          <p class="text-gray-700 mt-2">Nota: {{ number_format($movie['vote_average'], 1) }}</p>
+          <h2 class="text-lg font-bold">
+            {{ Str::substr($movie['title'], 0, 23) }}</h2>
+          <p class="mt-2 text-gray-700">Nota: {{ number_format($movie['vote_average'], 1) }}</p>
         </div>
       </li>
     </a>
