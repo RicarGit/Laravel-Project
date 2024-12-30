f080d94ebb8d6f359418652c5fa90dd90f93fd05
RicarGit <ricardoagava@gmail.com>
2024-12-30 17:25:37 -0300

    feat: adding buttons to login and sign up form pages

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,8 +2,13 @@
   <div class="container mx-auto flex items-center justify-between px-5">
     <a href="{{ route('home') }}" class="text-2xl font-bold">Filmes App</a>
     <div class="flex items-center gap-4">
-      <p>Usuário</p>
-      <div class="size-10 rounded-[50%] bg-gray-700"></div>
+      <a href="{{ route('login') }}"
+        class="rounded-md border-2 border-blue-500 px-4 py-2 font-bold uppercase text-blue-500"
+      >Entrar</a>
+      <a href="{{ route('sign-up') }}" class="rounded-md bg-blue-500 px-4 py-2 uppercase text-white">Criar Conta</a>
+      {{-- <p>Usuário</p>
+      <div class="size-10 rounded-[50%] bg-gray-700"></div> --}}
     </div>
+  </div>
   </div>
 </header>
